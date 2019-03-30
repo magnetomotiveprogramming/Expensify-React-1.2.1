e756331f9ac087a77b0de0b2cebd3494b440fae2
Nasrul Azim <magnetomotiveprogramming@gmail.com>
2019-03-30 17:00:21 +0800

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import {addExpense, removeExpense, editExpense} from './actions/expenses';
-import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+// import {addExpense, removeExpense, editExpense} from './actions/expenses';
+// import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css'
 import './styles/style.scss';
@@ -14,26 +14,26 @@ import 'react-dates/lib/css/_datepicker.css';
   
 const store = configureStore();
 
-store.dispatch(addExpense({ 
-  description: 'Water bill', 
-  amount : 100
-}));
-store.dispatch(addExpense({ 
-  description: 'Gas bill', 
-  amount : 100,
-  createdAt : 1000
-}));
-store.dispatch(addExpense({ 
-  description: 'rent', 
-  amount : 109500
-}));
+// store.dispatch(addExpense({ 
+//   description: 'Water bill', 
+//   amount : 100
+// }));
+// store.dispatch(addExpense({ 
+//   description: 'Gas bill', 
+//   amount : 100,
+//   createdAt : 1000
+// }));
+// store.dispatch(addExpense({ 
+//   description: 'rent', 
+//   amount : 109500
+// }));
 
 
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-console.log(visibleExpenses);
+// console.log(visibleExpenses);
 
 
 //The prop store equals the store variable on line 14
