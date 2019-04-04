077845c5d5dda6c34b6056d70d482c931af9352e
Nasrul Azim <magnetomotiveprogramming@gmail.com>
2019-04-04 19:12:09 +0800

    Redirecting Login or Logout

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //The import below connects react to redux
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 // import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from './actions/filters';
 // import getVisibleExpenses from './selectors/expenses';
 
@@ -21,18 +22,31 @@ const jsx = (
     <AppRouter />
   </Provider>
 )
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+}
 
 ReactDOM.render(<p>...Loading</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(()=>{
-  ReactDOM.render(jsx, document.getElementById('app'));
-})
+
 
 firebase.auth().onAuthStateChanged((user)=>{
   if(user){
-    console.log('log in')
+    store.dispatch(login(user.uid))
+    store.dispatch(startSetExpenses()).then(()=>{
+      renderApp();
+      if(history.location.pathname==='/'){
+        history.push('/dashboard');
+      }
+    })
   } else {
-    console.log('log out')
+    store.dispatch(logout())
+    renderApp();
+    history.push('/');
   }
 });
 
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
-test('should render Header correctly',()=>{
-  const wrapper = shallow(<Header />)
+test('should render Header correctly', () => {
+  const wrapper = shallow(<Header startLogout={() => { }} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
+});
+
 
 //this test originally uses react-test-renderer. It allows us to virtually render our components inside of just regular javascript code and then we can assert something about what got rendered. react-test-renderer is a react library created by the react team.
 
